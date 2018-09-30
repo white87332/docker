b502e97aba7dcd50235b8bebb4696b1a9cba4f7d
Tony <white87332@gmail.com>
2018-09-30 17:51:12 +0800

    update

--- wordpress/run.sh
+++ wordpress/run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-wget -c http://wordpress.org/latest.tar.gz \
-tar zxvf latest.tar.gz \
-rsync -av wordpress /var/www/html/ \
-rm latest.tar.gz \
-chown -R www-data:www-data /var/www/html \
-chmod -R 755 /var/www/html/
-
-service nginx start
-service php7.2-fpm start
+service nginx restart
+service php7.2-fpm restart
