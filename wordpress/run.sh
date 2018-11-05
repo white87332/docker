012617e11e26a44be33c3ab2613481c499040088
Tony <white87332@gmail.com>
2018-11-05 21:21:38 +0800

    update wordpress path

--- wordpress/run.sh
+++ wordpress/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+wget -c http://wordpress.org/latest.tar.gz
+tar zxvf latest.tar.gz
+rsync -av wordpress /var/www/html/
+rm latest.tar.gz
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html/
+
 service php7.2-fpm start
 service nginx stop
 nginx -g "daemon off;"
